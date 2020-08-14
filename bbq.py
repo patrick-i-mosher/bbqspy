4377d2458f4fa0feee423e81b129ee766249f303
Ian Mosher <patrick.i.mosher@gmail.com>
2020-08-14 10:19:57 -0400

    update

--- bbq.py
+++ bbq.py
@@ -1,23 +1,38 @@
-#!/usr/bin/python3
+#!/bin/python3
 
-from bluetooth.ble import DiscoveryService
-
-
+import Adafruit_BluefruitLE
 '''
 temps = [14, 1]
 magic = 0
 
 print((temps[magic + 1] << 8) | (temps[magic + 0] & 255)) 
 '''
-def do_scan(timeout=10):
-    print("Discovering devices")
-    service = DiscoveryService
+'''
+def ble_beacon_scan():
+   
+    request = GATTRequester(dest_addr)
+    value = request.read_by_handle(0x30)[0]
+    print("Got {}".format(value))
+   
+    print("Scanning for beacon")
+    service = BeaconService()
+    devices = service.scan(5)
+    for address, data in list(devices.items()):
+        b = Beacon(data, address)
+        print(b)
+
+def ble_scan(timeout=10):
+    print("Discovering nearby devices")
+    service = DiscoveryService("hci0")
     devices = service.discover(timeout)
-    for name, addr in devices.item:
-        print("{} - {}").format(addr, name)
-
+    for addr, name in devices.items():        
+        if name == "iBBQ":
+            return addr
+'''
 def main():
-    do_scan()
+    adapter = ble.get_default_adapter()
+    return
 
-if __name__ == '__main__':
-    main()
+ble = Adafruit_BluefruitLE.get_provider()
+ble.initialize()
+ble.run_mainloop_with(main)
