a9cd1a9f95da231238ce8ab725f67759a568b623
Patrick Mosher <patrick.i.mosher@gmail.com>
2020-08-17 13:54:43 -0400

    completed BLE component.

--- bbq.py
+++ bbq.py
@@ -1,36 +1,75 @@
-#!/bin/python3
+#!/usr/bin/python3
 
-import Adafruit_BluefruitLE
-'''
-temps = [14, 1]
-magic = 0
+from bluepy.btle import *
+import queue
+from threading import Thread
+import time
 
-print((temps[magic + 1] << 8) | (temps[magic + 0] & 255))
-'''
-'''
-def ble_beacon_scan():
-    request = GATTRequester(dest_addr)
-    value = request.read_by_handle(0x30)[0]
-    print("Got {}".format(value))
-    print("Scanning for beacon")
-    service = BeaconService()
-    devices = service.scan(5)
-    for address, data in list(devices.items()):
-        b = Beacon(data, address)
-        print(b)
+results_q = queue.Queue()
 
-def ble_scan(timeout=10):
-    print("Discovering nearby devices")
-    service = DiscoveryService("hci0")
-    devices = service.discover(timeout)
-    for addr, name in devices.items():
-        if name == "iBBQ":
-            return addr
-'''
+class MyDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleNotification(self, cHandle, data):
+        print("Got Notification for handle {}!".format(hex(cHandle)))
+        if cHandle == 0x30:
+            results = normalize_temps(data)
+            q.put(results)
+            '''
+            i = 1
+            for res in results:
+                print("Probe {}: {}F".format(i, res))
+
+                i += 1
+            '''
+def send_write_request(p, svc_uuid, char_uuid, notify_handle, data):
+    p.writeCharacteristic(notify_handle, data, withResponse=True)
+
+def normalize_temps(temp_array):
+    temps = []
+    for i in range(0,7,2):
+        temp = (temp_array[i + 1] <<8) | (temp_array[i] & 255)
+        temp /= 10
+        # Convert to F
+        f_temp = (temp * 9/5) + 32
+        if f_temp == 11826.68:
+            # No probe plugged in this slot
+            continue
+        temps.append((f_temp, datetime.datetime.now().timestamp()))
+    return temps
+
+def receive_notifications(p):
+    while True:
+        if p.waitForNotifications(1.0):
+            continue
+
+def setup_ble_connection():
+    print("Connecting")
+    # device MAC hardcoded here for now, may write code to scan 
+    # and get it programatically later
+    p = Peripheral("24:7d:4d:6b:e8:cc")
+    print("Connected")
+    p.setDelegate(MyDelegate())
+    service_uuid = 0xfff0
+    # Auto-pair
+    send_write_request(p, service_uuid, 0xfff2, 0x0029,
+                   b'\x21\x07\x06\x05\x04\x03\x02\x01\xb8\x22\x00\x00\x00\x00\x00')
+    # Register for notifications for temperature beacons
+    send_write_request(p, service_uuid, 0xfff4, 0x0031, b'\x01\x00')
+    # Tell device to start broadcasting temp notifications
+    send_write_request(p, service_uuid, 0xfff5, 0x0034, b'\x0b\x01\x00\x00\x00\x00')
+    return p;
+
 def main():
-    adapter = ble.get_default_adapter()
-    return
+    iBBQ = setup_ble_connection()
+    ble_notification_handler = Thread(target=receive_notifications,
+                                      args=(iBBQ))
+    ble_notification_handler.setDaemon(True)
+    ble_notification_handler.start()
+    '''
+    MATPLOTLIB animation stuff goes here
+    '''
 
-ble = Adafruit_BluefruitLE.get_provider()
-ble.initialize()
-ble.run_mainloop_with(main)
+if __name__ == '__main__':
+    main()
